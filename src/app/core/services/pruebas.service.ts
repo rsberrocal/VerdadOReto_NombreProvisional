d1be066956994e08da1ea2ce1438eb4fa6abeeb8
rsberrocal <rsberrocal@gmail.com>
2019-06-29 01:32:00 +0200

    Added pruebas export

--- src/app/core/services/pruebas.service.ts
+++ src/app/core/services/pruebas.service.ts
@@ -1,14 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Pruebas} from '../../shared/classes/pruebas';
 
+// @ts-ignore
+import * as pruebasConfig from '../../config/pruebas.config.json';
+
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PruebasService {
-  pruebasList: Pruebas[] = [];
-  constructor() { }
+    pruebasList: Pruebas[] = [];
 
-  loadPruebas(){
-    
-  }
+    constructor() {
+    }
+
+    loadPruebas() {
+        this.pruebasList = pruebasConfig.Pruebas as Pruebas[];
+        for(let t of this.pruebasList){
+            console.log(t);
+        }
+        console.log('json', pruebasConfig.Pruebas);
+        console.log('array', this.pruebasList);
+    }
 }
