77f487ed1a9c74d3cd3bf2bd4419b92309e91c53
Okabe <sergixghost@gmail.com>
2019-07-02 03:24:33 +0200

    Ya se actualiza y se muestran los usuarios secundarios de las pruebas.

--- src/app/pages/pruebas/pruebas.page.ts
+++ src/app/pages/pruebas/pruebas.page.ts
@@ -13,26 +13,24 @@ import {CurrentPruebaService} from "../../core/services/current-prueba.service";
 export class PruebasPage implements OnInit {
     prueba: Pruebas;
     jugadorP: Users;
-    jugadoresS: Users[];
+    jugadoresS: Users[] = [];
     scoreP: number;
     ronda: number;
     pruebaType = ["success", "RubenTonto"];
+    newDescript: string;
 
     constructor(private userService: UsersService, private pruebasService: PruebasService, private currentPrueba: CurrentPruebaService) {
         this.ronda = this.pruebasService.getRonda();
         this.scoreP = currentPrueba.getScore();
-        console.log(this.scoreP)
         this.jugadorP = currentPrueba.getCurrentUser();
         this.prueba = this.setPrueba();
-        this.pruebaType= this.pruebaTypeSetter();
+        this.setSecondaryPlayers();
+        this.replaceDescription();
+        this.pruebaType = this.pruebaTypeSetter();
 
     }
 
     ngOnInit() {
-    }
-
-    getUsersList(): Users[] {
-        return this.userService.getUsersList();
     }
 
     getPruebasList(): Pruebas[] {
@@ -52,24 +50,59 @@ export class PruebasPage implements OnInit {
         }
 
     }
-    pruebaTypeSetter(): string[]{
-        switch(this.scoreP){
+
+    pruebaTypeSetter(): string[] {
+        switch (this.scoreP) {
             case 1:
                 return ["primary", "Verdad"];
 
             case 2:
-                return ["success", "UPC"];
+                return ["success", "Reto Nivel UPC"];
 
             case 3:
-                return ["warning", "Medium"];
+                return ["warning", "Reto Nivel Medium"];
 
             case 4:
-                return ["danger", "UB"]
+                return ["danger", "Reto Nivel UB"]
 
         }
     }
 
-    setCurrentPlayer(user: Users, x: number) {
-
+    setSecondaryPlayers() {
+        let x = this.prueba.numPlayers - 1;
+        let bol;
+        for (let i = 0; i < x; i++) {
+            bol = false;
+            while (!bol) {
+                let jug = this.getRandomUser();
+                if (!this.jugadoresS.includes(jug) && (jug != this.jugadorP)) {
+                    this.jugadoresS.push(jug);
+                    bol = true;
+                }
+            }
+        }
     }
+
+    getUsers(): Users[] {
+        return this.userService.getUsersList();
+    }
+
+    getRandomUser(): Users {
+        let freeUser = false;
+        while (freeUser == false) {
+            let x = Math.floor(Math.random() * this.getUsers().length);
+            if (this.getUsers()[x].havePlayed == false) {
+                return this.getUsers()[x];
+            }
+        }
+    }
+
+    replaceDescription(){
+        let oldstr= this.prueba.description
+        for(let i= 0; i < this.prueba.numPlayers-1;i++) {
+            oldstr = oldstr.toString().replace("{user}", this.jugadoresS[i].name);
+        }
+        this.newDescript= oldstr;
+    }
+    //Aqui va el metodo para pasar a la siguiente ronda.
 }
