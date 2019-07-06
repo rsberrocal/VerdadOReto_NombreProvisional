3cfe58ee144f9628bf5d64708217bd28b13f088b
rsberrocal <rsberrocal@gmail.com>
2019-07-06 22:02:56 +0200

    EL PUTO RANKING NO VA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,22 @@ import {Component} from '@angular/core';
 import {MenuController, Platform} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
-import {PruebasService} from './core/services/pruebas.service';
+import {Users} from './shared/classes/users';
+import {UsersService} from './core/services/users.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html'
 })
 export class AppComponent {
+    users: Users[] = [];
+
     constructor(
         private platform: Platform,
         private splashScreen: SplashScreen,
         private statusBar: StatusBar,
         private menuController: MenuController,
-        //private users: User
+        private userService: UsersService
     ) {
         this.initializeApp();
     }
@@ -27,11 +30,11 @@ export class AppComponent {
         });
     }
 
-    disableMenu(event){
-        this.menuController.enable(false,'ranking');
+    disableMenu(event) {
+        this.menuController.enable(false, 'ranking');
     }
 
-    setRanking(){
-
+    setRanking() {
+        this.users = this.userService.getUsersList();
     }
 }
--- src/app/shared/components/ranking/ranking.component.ts
+++ src/app/shared/components/ranking/ranking.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Users} from '../../classes/users';
 import {UsersService} from '../../../core/services/users.service';
 
@@ -7,24 +7,23 @@ import {UsersService} from '../../../core/services/users.service';
     templateUrl: './ranking.component.html',
     styleUrls: ['./ranking.component.scss'],
 })
-export class RankingComponent implements OnInit {
+export class RankingComponent implements OnInit,OnChanges {
 
-    users: Users[] = [];
+    @Input()usersList: Users[] = [];
 
     constructor(private userService: UsersService) {
     }
 
     ionViewWillEnter() {
         console.log('setting');
-        this.setUsers();
     }
 
-    setUsers() {
-        this.users = this.userService.getUsersList();
-        console.log('users',this.users);
-    }
 
     ngOnInit(): void {
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log('eqwe',changes);
     }
 
 }
