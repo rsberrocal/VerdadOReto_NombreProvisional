16259e95af09111e7c62de7e14602d28dbccb712
Okabe <sergixghost@gmail.com>
2019-07-07 23:57:12 +0200

    Cosas de users

--- src/app/pages/users/users.page.ts
+++ src/app/pages/users/users.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Users} from "../../shared/classes/users";
+import {UsersService} from "../../core/services/users.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-users',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.page.scss'],
 })
 export class UsersPage implements OnInit {
+  users: Users[];
 
-  constructor() { }
+  constructor(private userService: UsersService, private router: Router) {
+      this.users = this.userService.userList.sort(function (a, b) {
+          return b.score - a.score
+      });
+  }
 
   ngOnInit() {
   }
+    navigate(routerLink) {
+        this.router.navigate([routerLink], {replaceUrl: true});
+        //Aqui va el metodo para pasar a la siguiente ronda.
+    }
 
 }
